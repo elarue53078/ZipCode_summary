a1cc2ba1267d5ebcc8879435728605f6c0fc2d36
Eric LaRue <safespace53078@gmail.com>
2019-11-01 15:41:46 -0500

    Editing code to eliminate errors

--- ZipCode_summary/Summary.cs
+++ ZipCode_summary/Summary.cs
@@ -7,7 +7,6 @@ namespace ZipCode_summary
     public class Summary
     {
         Dictionary<string, int> zcSummary = new Dictionary<string, int>();
-        string address;
 
         void CountZipCodes(string address)
         {
@@ -33,7 +32,7 @@ namespace ZipCode_summary
             var writer = new StreamWriter(@"path\zcSummary.csv");
             foreach (var pair in zcSummary)
             {
-                writer.WriteLine("{0},{1}", pair.Key, String.Join(",", pair.Value.Select(x => x.ToString()).ToArray()));
+                writer.WriteLine("{0},{1}", pair.Key, String.Join(",", pair.Value.ToString()));
             }
         }
     }
